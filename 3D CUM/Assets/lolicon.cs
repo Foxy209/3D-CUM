a83b220596ab54b1d76cbadfe9da516e073f76d0
Foxy209 <timur.tmn@mail.ru>
2021-02-21 19:05:27 +0500

    cum
    I CUM IN A ARTEM-GIRL

---
+++ 3D CUM/Assets/lolicon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lolicon : MonoBehaviour
+{
+    public Animator anime;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anime.SetTrigger("1122");
+        }
+    }
+}
